e938367d77dbec94a2c646131fe1e32d8cfdd642
@ChristianAbad_ <christianmillan179@gmail.com>
2022-02-22 17:34:29 +0100

    Códigos de Error

--- P1/Tienda/main.js
+++ P1/Tienda/main.js
@@ -13,27 +13,58 @@ console.log('Escuchando...');
 const server = http.createServer((req, res) => {
 
     // Peticion Recibida
-    console.log('!Peticion Recibida!');
+    console.log('¡Peticion Recibida!');
 
     /* Creación o construcción de objetos de tipo URL */
     const myURL = new URL(req.url, 'https://' + req.headers['host']);
     console.log('La URL obtenida es: ' + myURL.pathname);
 
-    // Cabecera del tipo de datos + Cuerpo de la Respuesta.
-    res.setHeader('Content-Type', 'text/plain');
+    // Buffer de variables
+    let filename = "";
 
-    // Envio de respuestas
-    res.write('Soy el Happy Server!! \n');
+    // Obtención de ruta (pathname) + Comprobación de ruta para su posterior devolución
+    if (myURL.pathname == '/'){
+        filename == "tienda.html"; // Página Principal --> [HOME]
+        file_content = (myURL.pathname).split(["."])[1];
+        content_type = "text/" + content;
+      }else{
+        file_content = (myURL.pathname).split(["."])[1]; // Extracción de path ubicado entre puntos '.'
+        content_type = "text/" + file_content; // Contenido de la página web 'myURL.pathname'
+        filename = "." + myURL.pathname; // Página que ha pedido el cliente
+      }
 
-    res.end();
+    // Imprime por pantalla la URL pedida por el cliente/usuario.
+    console.log('La URL pedida es correcta: ' + filename);
 
+    // El contenido mostrado por la pantalla de la URL pedida por el cliente es el siguiente.
+    console.log('El contenido del recurso es: ' + file_content);
+
+    // Lectura Asincrona    
+    fs.readFile(filename, function(err, data){
+        // En caso de ¡ERROR! --> Página No Encontrada
+        if (err){
+            console.log('¡ERROR!, Pagina No Encontrada');
+            let code = 404;
+            let code_msg = "Not Found";
+            data = fs.readFileSync('error.html', 'utf-8');
+            content = (myURL.pathname).split(["."])[1]
+            content_type = "text/" + content;
+            res.statusCode = code;
+            res.statusMessage = code_msg;
+            res.setHeader('Content-Type', content_type);
+            res.write(data);
+            return res.end();
+        }
+        
+        console.log(content_type)
+        let code = 200;
+        let code_msg = "OK";
+        res.statusCode = code;
+        res.statusMessage = code_msg;
+        res.write(data);
+        res.end();
+    })
 });
-
-/* Creación o construcción de objetos de tipo URL */
-const myURL = new URL(req.url, 'https://' + req.headers['host']);
-
-/* Imprimir el objeto URL para ver todas sus partes en la consola de comandos */
-console.log(myURL);
 
 // Puerto del servidor de escucha
 server.listen(PUERTO);
