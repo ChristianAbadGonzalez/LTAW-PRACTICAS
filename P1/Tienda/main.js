b6f3cded2addec5afe47ef7751b0d70766370993
@ChristianAbad_ <christianmillan179@gmail.com>
2022-02-22 16:09:16 +0100

    Aplicando S03 - Avance de JavaScript

--- P1/Tienda/main.js
+++ P1/Tienda/main.js
@@ -1,14 +1,23 @@
 // Importacion de modulos HTTP, FS, URL
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 // Definición del PUERTO que vamos a utilizar
-const PUERTO = 9090
+const PUERTO = 1704;
+
+// Servidor
+console.log('Escuchando...');
 
 // Creacion del Servidor Web con sus respectivos argumentos
 const server = http.createServer((req, res) => {
 
     // Peticion Recibida
-    console.log('Peticion Recibida');
+    console.log('!Peticion Recibida!');
+
+    /* Creación o construcción de objetos de tipo URL */
+    const myURL = new URL(req.url, 'https://' + req.headers['host']);
+    console.log('La URL obtenida es: ' + myURL.pathname);
 
     // Cabecera del tipo de datos + Cuerpo de la Respuesta.
     res.setHeader('Content-Type', 'text/plain');
@@ -19,6 +28,12 @@ const server = http.createServer((req, res) => {
     res.end();
 
 });
+
+/* Creación o construcción de objetos de tipo URL */
+const myURL = new URL(req.url, 'https://' + req.headers['host']);
+
+/* Imprimir el objeto URL para ver todas sus partes en la consola de comandos */
+console.log(myURL);
 
 // Puerto del servidor de escucha
 server.listen(PUERTO);
