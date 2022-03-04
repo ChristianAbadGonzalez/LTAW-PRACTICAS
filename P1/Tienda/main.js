3a15cbae054001df452b2695bac68fe868bb67e1
@ChristianAbad_ <christianmillan179@gmail.com>
2022-03-04 14:00:16 +0100

    SERVIDOR OPERATIVO

--- P1/Tienda/main.js
+++ P1/Tienda/main.js
@@ -4,7 +4,7 @@ const url = require('url');
 const fs = require('fs');
 
 // Definición del PUERTO que vamos a utilizar
-const PUERTO = 1704;
+const PUERTO = 9090;
 
 // Servidor
 console.log('Escuchando...');
@@ -20,13 +20,15 @@ const server = http.createServer((req, res) => {
     console.log('La URL obtenida es: ' + myURL.pathname);
 
     // Buffer de variables
-    let filename = "";
+    let filename = "",
+        file_content = "",
+        content_type = "",
+        code = 200;
 
     // Obtención de ruta (pathname) + Comprobación de ruta para su posterior devolución
     if (myURL.pathname == '/'){
-        filename == "tienda.html"; // Página Principal --> [HOME]
-        file_content = (myURL.pathname).split(["."])[1];
-        content_type = "text/" + content;
+        filename = "tienda.html"; // Página Principal --> [HOME]
+        content_type = "text/html";
       }else{
         file_content = (myURL.pathname).split(["."])[1]; // Extracción de path ubicado entre puntos '.'
         content_type = "text/" + file_content; // Contenido de la página web 'myURL.pathname'
@@ -39,32 +41,18 @@ const server = http.createServer((req, res) => {
     // El contenido mostrado por la pantalla de la URL pedida por el cliente es el siguiente.
     console.log('El contenido del recurso es: ' + file_content);
 
-    // Lectura Asincrona    
+    // Lectura Sincrona    
     fs.readFile(filename, function(err, data){
         // En caso de ¡ERROR! --> Página No Encontrada
         if (err){
-            console.log('¡ERROR!, Pagina No Encontrada');
-            let code = 404;
-            let code_msg = "Not Found";
-            data = fs.readFileSync('error.html', 'utf-8');
-            content = (myURL.pathname).split(["."])[1]
-            content_type = "text/" + content;
-            res.statusCode = code;
-            res.statusMessage = code_msg;
-            res.setHeader('Content-Type', content_type);
-            res.write(data);
-            return res.end();
+          res.writeHead(404, {'Content-Type': 'text/html'})
+          return res.end('¡ERROR:! FILE NOT FOUND');
         }
-        
-        console.log(content_type)
-        let code = 200;
-        let code_msg = "OK";
-        res.statusCode = code;
-        res.statusMessage = code_msg;
+        res.writeHead(code, {'Content-Type': content_type});
         res.write(data);
-        res.end();
+        return res.end();
+      });
     })
-});
 
 // Puerto del servidor de escucha
 server.listen(PUERTO);
