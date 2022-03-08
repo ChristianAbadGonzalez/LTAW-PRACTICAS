ca8626ea117d9c69a4e83302ed29f5b48ac93bcb
@ChristianAbad_ <christianmillan179@gmail.com>
2022-03-08 09:58:54 +0100

    Ejemplo 1 S05

---
+++ P2/S05 - EJEMPLOS/EJ1-Elemento-Tienda.js
@@ -0,0 +1,29 @@
+/* 
+   ENUNCIADO:
+   
+   Creamos el elemento tienda que contiene dos productos. 
+   Luego recorremos la estructura y mostrarmos en la consola información: 
+   
+*/
+
+//-- Crear la variable tienda, que es una estructura formada
+//-- por dos productos
+const tienda = [
+    {
+      nombre: "Alhambra II",
+      descripcion: "Placa con FPGA ice40HX8K",
+      stock: 3
+    },
+    {
+      nombre: "Icestick",
+      stock: 10
+    }
+  ];
+  
+  //-- Mostrar informacion sobre la tienda
+  console.log("Productos en la tienda: " + tienda.length);
+  
+  //-- Recorrer el array de productos
+  tienda.forEach((element, index)=>{
+    console.log("Producto: " + (index + 1) + ": " + element.nombre);
+  });
