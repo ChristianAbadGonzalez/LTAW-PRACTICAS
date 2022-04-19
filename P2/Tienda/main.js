3438ea0c24200669015d43a1e5c12a49c6519dee
ChristianAbadGonzalez <christianmillan179@gmail.com>
2022-04-19 16:57:57 +0200

    Servidor PP con Bootstrap

--- P2/Tienda/main.js
+++ P2/Tienda/main.js
@@ -1,214 +1,263 @@
-// Importacion de modulos HTTP, FS, URL
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+// Modulos utilizados
+const http = require('http'),
+      url = require('url'),
+      fs = require('fs'),
+      PUERTO = 9090
 
-// Definición del PUERTO que vamos a utilizar
-const PUERTO = 9090;
+// BASES DE DATOS, A LO CUTRE
+    // productos y precios
+let productos = {'Lenovo IdeaPad 3 15IGL05 8 GB Intel Celeron N4020 256 GB SSD': 399,
+                 'Asus Chromebook Flip C214MA-BU0410 4 GB Intel Celeron N4020 32 GB eMMC': 199,
+                 'Lenovo IdeaPad 3 Chromebook 4 GB Intel Celeron N4020 64 GB eMMC': 294.66,
+                 'HP Chromebook 14a-NA0004NS 4 GB Intel Celeron N4020 64 GB eMMC': 299,
+                 'Asus E410MA-EK007TS 4 GB Intel Celeron N4020 64 gb eMMC': 248.99,
+                 'Acer Extensa 15 EX215-22-R84H 8 GB AMD Ryzen 5 3500U 512 GB SSD': 508.22,
+                 'Lenovo V14 G2-ITL 82KA001BSP 8 GB Intel Core i3-1115G4 256 GB SSD': 513.10,
+                 'Asus ExpertBook L1500CDA-EJI480R 8 GB AMD Ryzen 3 3250U 256 GB SSD': 551.50,
+                 'Asus VivoBook Pro 15 M3500QC-KJ161T 16 GB AMD Ryzen 7 5800H NVIDIA RTX 3050 512 GB SSD': 999.99,
+                 'MSI GF63 Thin 11SC-440XES 8 GB Intel Core i5 11400H NVIDIA GTX 1650 512 GB SSD': 849.99,
+                 'MSI WS63 8SL-013ES Vpro 32 GB Intel Core i7-8850H NVIDIA P4200 512 GB SSD + 1TB HDD': 5930.85,
+                 'Asus Rog Strix SCAR 17 G733QS-K4170T 64 GB AMD Ryzen 9 5900HX NVIDIA 3080 1 TB SSD + 1 TB HDD': 3319.63,
+                 'HP ZBook Fury 17 G7 32 GB Intel Core i9-10885 NVIDIA Quadro RTX 5000 1 TB SSD': 5657.93,
+                 'Apple Macbook Pro 16 GB Intel Core i9 Radeon Pro 5500M 1 TB SSD': 3604.28,
+                 'Acer ConceptD 7 Pro CN715-72P-798Z 32 GB Intel Core i7-10875 NVIDIA Quadro RTX 5000 1 TB SSD': 3181.73,},
+    // ejemplo de cookie vacia
+    user_template = {'password': '',
+                     'cart':{
+                       'Lenovo IdeaPad 3 15IGL05 8 GB Intel Celeron N4020 256 GB SSD': 0,
+                       'Asus Chromebook Flip C214MA-BU0410 4 GB Intel Celeron N4020 32 GB eMMC': 0,
+                       'Lenovo IdeaPad 3 Chromebook 4 GB Intel Celeron N4020 64 GB eMMC': 0,
+                       'HP Chromebook 14a-NA0004NS 4 GB Intel Celeron N4020 64 GB eMMC': 0,
+                       'Asus E410MA-EK007TS 4 GB Intel Celeron N4020 64 gb eMMC': 0,
+                       'Acer Extensa 15 EX215-22-R84H 8 GB AMD Ryzen 5 3500U 512 GB SSD': 0,
+                       'Lenovo V14 G2-ITL 82KA001BSP 8 GB Intel Core i3-1115G4 256 GB SSD': 0,
+                       'Asus ExpertBook L1500CDA-EJI480R 8 GB AMD Ryzen 3 3250U 256 GB SSD': 0,
+                       'Asus VivoBook Pro 15 M3500QC-KJ161T 16 GB AMD Ryzen 7 5800H NVIDIA RTX 3050 512 GB SSD': 0,
+                       'MSI GF63 Thin 11SC-440XES 8 GB Intel Core i5 11400H NVIDIA GTX 1650 512 GB SSD': 0,
+                       'MSI WS63 8SL-013ES Vpro 32 GB Intel Core i7-8850H NVIDIA P4200 512 GB SSD + 1TB HDD': 0,
+                       'Asus Rog Strix SCAR 17 G733QS-K4170T 64 GB AMD Ryzen 9 5900HX NVIDIA 3080 1 TB SSD + 1 TB HDD': 0,
+                       'HP ZBook Fury 17 G7 32 GB Intel Core i9-10885 NVIDIA Quadro RTX 5000 1 TB SSD': 0,
+                       'Apple Macbook Pro 16 GB Intel Core i9 Radeon Pro 5500M 1 TB SSD': 0,
+                       'Acer ConceptD 7 Pro CN715-72P-798Z 32 GB Intel Core i7-10875 NVIDIA Quadro RTX 5000 1 TB SSD': 0,
+                       'total': 0.0}
+                     }
 
-// Servidor
-console.log('Escuchando...');
-
-//Definir tipos de mime
-const mime_type = {
-  "html" : "text/html",
-  "css"  : "text/css",
-  "js"   : "application/javascript",
-  "jpg"  : "image/jpg",
-  "JPG"  : "image/JPG",
-  "jpeg" : "image/jpeg",
-  "png"  : "image/png",
-  "PNG"  : "image/PNG",
-  "gif"  : "image/gif",
-  "ico"  : "image/icon",
-  "json" : "application/json",
-};
-
-//Página principal
-const TIENDA = fs.readFileSync('tienda.html', 'utf-8');
-
-//Página formulario
-const FLOGIN = fs.readFileSync('form-login.html', 'utf-8');
-
-//Páginas principales de cada ordenador
-const ORDENADOR1 = fs.readFileSync('ordenador1.html', 'utf-8');
-const ORDENADOR2 = fs.readFileSync('ordenador2.html', 'utf-8');
-const ORDENADOR3 = fs.readFileSync('ordenador3.html', 'utf-8');
-const ORDENADOR4 = fs.readFileSync('ordenador4.html', 'utf-8');
-const ORDENADOR5 = fs.readFileSync('ordenador5.html', 'utf-8');
-const ORDENADOR6 = fs.readFileSync('ordenador6.html', 'utf-8');
-const ORDENADOR7 = fs.readFileSync('ordenador7.html', 'utf-8');
-const ORDENADOR8 = fs.readFileSync('ordenador8.html', 'utf-8');
-const ORDENADOR9 = fs.readFileSync('ordenador9.html', 'utf-8');
-const ORDENADOR10 = fs.readFileSync('ordenador10.html', 'utf-8');
-const ORDENADOR11 = fs.readFileSync('ordenador11.html', 'utf-8');
-const ORDENADOR12 = fs.readFileSync('ordenador12.html', 'utf-8');
-const ORDENADOR13 = fs.readFileSync('ordenador13.html', 'utf-8');
-const ORDENADOR14 = fs.readFileSync('ordenador14.html', 'utf-8');
-const ORDENADOR15 = fs.readFileSync('ordenador15.html', 'utf-8');
-
-//Página error
-const ERROR = fs.readFileSync('error.html', 'utf-8');
-
-//-- Cargar pagina web del formulario 
-const FORMULARIO_PEDIDO = fs.readFileSync('form-pedido.html','utf-8');
-
-//Página respuesta Bienvenido a la tienda y cliente desconocido
-const LOGIN = fs.readFileSync('login.html', 'utf-8');
-const NOLOGIN = fs.readFileSync('no-login.html', 'utf-8');
-
-//Página usuario dentro de la tienda
-const LOGIN_USER = fs.readFileSync('user-login.html', 'utf-8');
-
-//Página comprar formulario y respuesta
-const PEDIDO_OK = fs.readFileSync('pedido-ok.html','utf-8');
-const ADD_OK = fs.readFileSync('producto-cesta.html', 'utf-8');
-
-//Página carrito
-const CARRITO = fs.readFileSync('carrito.html', 'utf-8');
-let carrito_existe = false;
-let busqueda; //Definición busqueda
-
-//JSON estructura de la tienda
-const FICHERO_JSON = "tienda.json";
-
-//Fichero JSON modificado
-const FICHERO_JSON_MOD = "tienda-modificada.json";
-
-//Lectura del fichero JSON
-const  tienda_json = fs.readFileSync(FICHERO_JSON);
-const tienda = JSON.parse(tienda_json);
-
-//Lista usuarios registrados.
-let users_reg = [];
-console.log("Lista de usuarios registrados");
-console.log("-----------------------------");
-tienda[1]["usuarios"].forEach((element, index)=>{
-    console.log("Usuario " + (index + 1) + ": " + element.user);
-    users_reg.push(element.user);
-  });
-console.log();
-
-//contraseña usuarios
-let password_reg = [];
-let usuarios_reg = tienda[1]["usuarios"];
-for (i = 0; i < usuarios_reg.length; i++){
-    users_reg.push(usuarios_reg[i]["usuario"]);
-    password_reg.push(usuarios_reg[i]["password"]);
-};
-
-//Lista productos disponibles.
-let productos_disp = [];
-let product_list = [];
-console.log("Lista de productos disponibles");
-console.log("-----------------------------");
-tienda[0]["productos"].forEach((element, index)=>{
-  console.log("Articulo " + (index + 1) + ": " + element.nombre +
-              ", Stock: " + element.stock + ", Precio: " + element.precio);
-  productos_disp.push([element.nombre, element.descripcion, element.stock, 
-                       element.precio]);
-  product_list.push(element.nombre);
-});
-console.log();
-
-//Analizar la cookie y devolver el nombre de usuario si existe
-function get_user(req) {
-  
-  //-- Leer la cookie recibida
-  const cookie = req.headers.cookie;
-
-  //Si hay cookie, guardamos el usuario
-  if (cookie) {
-    //Obtenemos array con todos los pares nombre-valor
-    let pares = cookie.split(";");
-
-    //Variable para guardar usuario
-    let user;
-
-    //Recorrer todos los pares nombre-valor
-    pares.forEach((element, index) => {
-      //Obtenemos los nombre y los valores por separado
-      let [nombre, valor] = element.split('=');
-
-      //Lee el usuario solo si nombre = user
-      if (nombre.trim() === 'user') {
-        user = valor;
-      }
-    });
-
-    //si user no asignada devuelve null
-    return user || null;
-  }
-}
-
-//Funcion para crear las cookies al añadir articulos al carrito
-function add_carrito(req, res, producto) {
-  const cookie = req.headers.cookie;
-
-  if (cookie) {
-    //Obteneos un array con todos los pares nombre-valor
-    let pares = cookie.split(";");
-
-    //Recorremos todos los pares nombre-valor
-    pares.forEach((element, index) => {
-      //Obtenemos los nombre y los valores por separado
-      let [nombre, valor] = element.split('=');
-
-      //Si nombre = carrito enviamos cookie de respuesta
-      if (nombre.trim() === 'carrito') {
-        res.setHeader('Set-Cookie', element + ':' + producto);
-      }
-    });
-  }
-}
-
-// Creacion del Servidor Web con sus respectivos argumentos
 const server = http.createServer((req, res) => {
+  let q = url.parse(req.url, true),
+      cookie = req.headers.cookie,
+      params = q.query,
+      filename = '',
+      mime = '',
+      ext = '',
+      prod_search = [],
+      prod_show = []
 
-    // Peticion Recibida
-    console.log('¡Peticion Recibida!');
+  if (q.pathname != '/') {
+    ext = q.pathname.split('.')[q.pathname.split('.').length - 1].toLowerCase()
+  }
 
-    /* Creación o construcción de objetos de tipo URL */
-    const myURL = new URL(req.url, 'https://' + req.headers['host']);
-    console.log('La URL obtenida es: ' + myURL.pathname);
-
-    // Buffer de variables
-    let filename = "",
-        file_content = "",
-        content_type = "",
-        code = 200;
-
-    // Obtención de ruta (pathname) + Comprobación de ruta para su posterior devolución
-    if (myURL.pathname == '/'){
-        filename = "tienda.html"; // Página Principal --> [HOME]
-        content_type = "text/html";
-      }else{
-        file_content = (myURL.pathname).split(["."])[1]; // Extracción de path ubicado entre puntos '.'
-        content_type = "text/" + file_content; // Contenido de la página web 'myURL.pathname'
-        filename = "." + myURL.pathname; // Página que ha pedido el cliente
+  switch (ext) {
+    case '':
+      filename = './layout/index.html'
+      mime = 'text/html'
+      code = 200
+      break
+    case 'register_form':
+      if (req.method == 'POST') {
+        req.on('data', chunk => {
+          data = chunk.toString()
+          console.log(data)
+          let new_user = true,
+              cname = '',
+              name = data.split('&')[0].split('=')[1]
+          if (cookie) {
+            // si existen cookies anteriores comprobamos si el usuario ya esta
+            // registrado
+            for (var i = 0; i < cookie.split('; ').length; i++) {
+              cname = cookie.split('; ')[i].split('=')[0]
+              if (cname == name) {
+                new_user = false
+                break
+              }
+            }
+          }
+          if (new_user) {
+            // si el usuario no estaba registrado, se le crea una nueva cookie
+            user = user_template
+            user.password = data.split('&')[1].split('=')[1]
+            res.setHeader('Set-Cookie', name + '=' + JSON.stringify(user))
+          }
+          return
+        })
       }
-
-    // Imprime por pantalla la URL pedida por el cliente/usuario.
-    console.log('La URL pedida es correcta: ' + filename);
-
-    // El contenido mostrado por la pantalla de la URL pedida por el cliente es el siguiente.
-    console.log('El contenido del recurso es: ' + file_content);
-
-    // Lectura Sincrona    
-    fs.readFile(filename, function(err, data){
-        // En caso de ¡ERROR! --> Página No Encontrada
-        if (err){
-          res.writeHead(404, {'Content-Type': 'text/html'})
-          return res.end('¡ERROR:! FILE NOT FOUND');
+      filename = './layout/tienda.html'
+      mime = 'text/html'
+      break
+    case 'cart': // PETICION GET: http://localhost:PORT/action.cart?user=NOMBRE
+      let cname = '',
+          sent = false
+      for (var i = 0; i < cookie.split('; ').length; i++) {
+        cname = cookie.split('; ')[i].split('=')[0]
+        if (cname == params.user) {
+          cart = JSON.parse(cookie.split(';')[i].split('=')[1])
+          content = JSON.stringify(cart.cart)
+          sent = true
         }
-        res.writeHead(code, {'Content-Type': content_type});
-        res.write(data);
-        return res.end();
-      });
+      }
+      if (!sent) {
+        content = ''
+      }
+      res.setHeader('Content-Type', 'application/json')
+      res.write(content)
+      res.end()
+      break
+    case 'pay':
+      if (req.method == 'POST') {
+        req.on('data', chunk => {
+          data = chunk.toString()
+          let cname = '',
+              name = data.split('&')[0].split('=')[1],
+              password = data.split('&')[1].split('=')[1]
+          for (var i = 0; i < cookie.split('; ').length; i++) {
+            cname = cookie.split('; ')[i].split('=')[0]
+            if (cname == name) {
+              user = JSON.parse(cookie.split('; ')[i].split('=')[1])
+              if (password == user.password) {
+                user.cart = user_template.cart
+                res.setHeader('Set-Cookie', name + '=' + JSON.stringify(user))
+                filename = './layout/compra-ok.html'
+              } else {
+                filename = './layout/error.html'
+              }
+            }
+          }
+          fs.readFile(filename, (err, data) => {
+            if (err) {
+              res.writeHead(404, {'Content-Type': 'text/html'})
+              res.write('<h1>Error 404: File not Found</h1>')
+              return res.end()
+            } else {
+              res.writeHead(200, {'Content-Type': 'text/html'})
+              res.write(data)
+              return res.end()
+            }
+          })
+          return
+        })
+      }
+      break
+    case 'buy':
+      if (req.method == 'POST') {
+        req.on('data', chunk => {
+          let data = chunk.toString(),
+              content = '',
+              cname = '',
+              name = data.split('&')[0].split('=')[1],
+              prod = data.split('&')[1].split('=')[1].replace(/[+]/gi,' ')
+          if (cookie) {
+            for (var i = 0; i < cookie.split('; ').length; i++) {
+              cname = cookie.split('; ')[i].split('=')[0]
+              if (cname == name) {
+                content = name + '='
+                let cart = JSON.parse(cookie.split('; ')[i].split('=')[1])
+                cart.cart[prod] += 1
+                cart.cart.total = Math.round((cart.cart.total + productos[prod])*100)/100
+                content += JSON.stringify(cart)
+              }
+            }
+          }
+          if (content) {
+            res.setHeader('Set-Cookie', content)
+            filename = './layout/tienda.html'
+          } else {
+            filename = './layout/registro.html'
+          }
+          fs.readFile(filename, (err, data) => {
+            if (err) {
+              res.writeHead(404, {'Content-Type': 'text/html'})
+              res.write('<h1>Error 404: File not Found</h1>')
+              return res.end()
+            } else {
+              res.writeHead(200, {'Content-Type': 'text/html'})
+              res.write(data)
+              return res.end()
+            }
+          })
+          return
+        })
+      }
+      break
+    
+    case 'js':
+      filename = './static/js' + q.pathname
+      mime = 'application/javascript'
+      break;
+    
+    case 'html':
+      filename = './layout' + q.pathname
+      mime = 'text/html'
+      break
+    
+    case 'searchbar': // PETICION GET: http://localhost:PORT/action.searchbar?prod=PRODUCTO
+    
+    case 'showsearch': // PETICION GET: http://localhost:PORT/action.showsearch?prod=PRODUCTO
+      for (let prod in productos) {
+        if (params.prod.length > 0) {
+          if (prod.toLowerCase().indexOf(params.prod.toLowerCase()) == -1) {
+            prod_show.push(prod)
+          } else {
+            prod_search.push(prod)
+          }
+        }
+      }
+      content = (ext == 'showsearch') ? JSON.stringify(prod_show) : JSON.stringify(prod_search)
+      res.setHeader('Content-Type', 'application/json')
+      res.write(content)
+      res.end()
+      break
+    
+    case 'jpg':
+    
+    case 'png':
+    
+    case 'ico':
+      filename = './static/images' + q.pathname
+      mime = 'images/' + ext
+      break;
+    
+    case 'show_cart':
+      filename = './layout/cart.html'
+      mime = 'text/html'
+      break;
+    
+    case 'css':
+      filename = './static/css' + q.pathname
+      mime = 'text/css'
+      break;
+    
+    case 'ttf':
+      filename = './static/css' + q.pathname
+      mime = 'font/ttf'
+      break;
+    default:
+      //
+  }
+  
+  console.log('request: ' + q.pathname + '\n\n')
+  
+  if (q.pathname.toLowerCase().indexOf('action.') == -1 || q.pathname.toLowerCase().indexOf('show_cart') != -1 || q.pathname.toLowerCase().indexOf('register_form') != -1) {
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.write('<h1>Error 404: File not Found</h1>')
+        return res.end()
+      } else {
+        res.writeHead(200, {'Content-Type': mime})
+        res.write(data)
+        return res.end()
+      }
     })
+  }
+}).listen(PUERTO)
 
-// Puerto del servidor de escucha
-server.listen(PUERTO);
-
-console.log('Servidor Activado. Escuchando en el Puerto: ' + PUERTO);
-
+console.log('Servidor en: http://localhost:' + PUERTO + '/')
